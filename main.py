83f636868970d65a6cf9bbd084f77e5e07fa8a58
Vinny D <thevinnycast@gmail.com>
2021-11-21 23:36:27 -0800

    init

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+import pandas as pd
+
+file = "c211979467978047498.csv"
+channel_df = pd.read_csv(file, index_col="ID")
+
+length = []
+for message in channel_df["Contents"]:
+    length.append(len(message))
+channel_df["Message Length"] = length
+
+for attr in channel_df:
+    print(channel_df[attr])
