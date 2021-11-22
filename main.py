d421e222a2dd00a1a7aaeec26732b5b1d6fb0795
Kevin Dang <kevinthedang_1@outlook.com>
2021-11-22 15:58:43 -0800

    comments on attributes

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pandas as pd
 
-file = "c211979467978047498.csv"
+file = "messages.csv"
 channel_df = pd.read_csv(file, index_col="ID")
 
 length = []
 for message in channel_df["Contents"]:
-    length.append(len(message))
+    length.append(len(str(message)))
 channel_df["Message Length"] = length
 
 for attr in channel_df:
     print(channel_df[attr])
+
+'''
+Current Attributes:
+    1. Timestamp
+    2. Contents
+    3. Attachments
+    4. Message Length
+    5. ID
+'''
