86078298190e94a87056bfb96b596f8982960a71
Vinny D <thevinnycast@gmail.com>
2021-11-22 16:08:07 -0800

    punct count processing implemented

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ file = "messages.csv"
 channel_df = pd.read_csv(file, index_col="ID")
 
 length = []
+punct = []
 for message in channel_df["Contents"]:
-    length.append(len(str(message)))
+    msg = str(message)
+    length.append(len(msg))
+    count = 0
+    if "." in msg or "!" in msg or "?" in msg:
+        count += msg.count(".") + msg.count("!") + msg.count("?")
+    punct.append(count)
 channel_df["Message Length"] = length
+channel_df["Punctuation Count"] = punct
 
 for attr in channel_df:
     print(channel_df[attr])
 
 '''
 Current Attributes:
-    1. Timestamp
-    2. Contents
-    3. Attachments
-    4. Message Length
-    5. ID
+    1. ID
+    2. Timestamp
+    3. Contents
+    4. Attachments
+    5. Message Length
+    6. Punctuation Count
 '''
