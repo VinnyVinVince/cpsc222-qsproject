1735b50c0a6abee769f25879aaa697adb5b17f5f
Vinny D <thevinnycast@gmail.com>
2021-11-23 12:13:45 -0800

    prelim hypo testing values

--- main.py
+++ main.py
@@ -53,7 +53,33 @@ def graph_punct(file):
     plt.show()
 
 
+'''
 f_name = "messages"
 clean_data(f_name)
 graph_length(f_name)
-#graph_punct(f_name)
+graph_punct(f_name)
+'''
+
+'''
+Hypothesis: Average length of Kev messages less than average length of Vin messages
+Null: Kev Mean >= Vin Mean
+Alt: Kev Mean < Vin Mean
+Sig Level: 5% (a=0.05)
+t-critical: 1.645
+
+If t < 1.645, reject null
+If t >= 1.645, don't reject null
+'''
+kev_df = pd.read_csv("messages_cleaned.csv", index_col="ID")
+vin_df = pd.read_csv("c298044618145136640_cleaned.csv", index_col="ID")
+df = len(kev_df) + len(vin_df) - 2
+print("Degrees of freedom:", df)
+
+t, p = stats.ttest_ind(kev_df["Message Length"], vin_df["Message Length"])
+print("t-value:", t)
+print("p-value:", p / 2)
+
+'''
+t = 1.21 < 1.645, reject null
+p = 0.11 > 0.05, don't reject null
+'''
